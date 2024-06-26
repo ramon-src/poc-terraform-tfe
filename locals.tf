fd5872f9671ab0f588b3e01d741f7d89dbaf66ba
Ramon Schmidt <ramonroc@gmail.com>
2024-06-26 10:48:45 -0300

    feat: add new local github workspace

--- locals.tf
+++ locals.tf
@@ -12,7 +12,13 @@ locals {
             description = "Example description of workspace"
             execution_mode = "remote"
             project_id = module.project["poc-terraform-project"].id
-            vcs_repo_identifier = "ramon-src/poc-terraform-tfe"
+            vcs_repo_identifier = "${var.github_organization_name}/poc-terraform-tfe"
+        }
+        "poc-github-workspace" = {
+            description = "Example description of workspace"
+            execution_mode = "local"
+            project_id = module.project["poc-terraform-project"].id
+            vcs_repo_identifier = "${var.github_organization_name}/poc-terraform-github"
         }
     }
 }
